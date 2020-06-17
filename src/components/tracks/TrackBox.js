11e6b9934938aa0d09ac3b6679d0b7596ed9e169
kojidelmundo <koji@tanda.co>
2020-06-17 16:48:58 +0800

    fix track iframe url

--- src/components/tracks/TrackBox.js
+++ src/components/tracks/TrackBox.js
@@ -2,12 +2,11 @@ import React from "react"
 import "./TrackBox.css"
 
 function TrackBox({ track, useIframe }) {
-  const uri = track.uri.split(":")[2]
   return(
     <div className={"TrackBox" + (useIframe ? "" : " spoopi-track-box")}>
       {
         useIframe
-        ? <iframe className="track-iframe" title={track.id} src={"https://open.spotify.com/embed/track/" + uri} frameborder="" allowtransparency="true" allow="encrypted-media"></iframe>
+        ? <iframe className="track-iframe" title={track.id} src={"https://open.spotify.com/embed/track/" + track.id} frameborder="" allowtransparency="true" allow="encrypted-media"></iframe>
           : <><img className="track-image" src={track.image_url} alt={track.id}/>
           <div className="track-name">{track.name}</div>
           <div className="track-artists">{track.artists.join(", ")}</div></>
