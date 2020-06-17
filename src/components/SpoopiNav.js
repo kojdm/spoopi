830e18b0fc8a382d7c3ccc946a796aeaca3580fb
kojidelmundo <koji@tanda.co>
2020-06-17 18:23:47 +0800

    add cursive font

--- src/components/SpoopiNav.js
+++ src/components/SpoopiNav.js
@@ -4,7 +4,7 @@ import "./SpoopiNav.css"
 function SpoopiNav() {
   return(
     <div className="SpoopiNav">
-      <div className="spoopi-logo">Spoopi</div>
+      <div className="spoopi-logo cursive">spoopi</div>
       <div className="spoopi-description">(spo)tify (p)laylist t(i)mer</div>
     </div>
   )
