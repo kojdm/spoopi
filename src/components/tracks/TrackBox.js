45fffcf7ba5f15c010495916e6476851b876f948
kojidelmundo <koji@tanda.co>
2020-06-17 16:37:58 +0800

    fix track iframe css class name

--- src/components/tracks/TrackBox.js
+++ src/components/tracks/TrackBox.js
@@ -7,7 +7,7 @@ function TrackBox({ track, useIframe }) {
     <div className={"TrackBox" + (useIframe ? "" : " spoopi-track-box")}>
       {
         useIframe
-        ? <iframe className="iframe" title={track.id} src={"https://open.spotify.com/embed/track/" + uri} frameborder="" allowtransparency="true" allow="encrypted-media"></iframe>
+        ? <iframe className="track-iframe" title={track.id} src={"https://open.spotify.com/embed/track/" + uri} frameborder="" allowtransparency="true" allow="encrypted-media"></iframe>
           : <><img className="track-image" src={track.image_url} alt={track.id}/>
           <div className="track-name">{track.name}</div>
           <div className="track-artists">{track.artists.join(", ")}</div></>
