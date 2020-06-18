ebc7b990b9a0fa3f4b5e964caef4f798cf76a6b4
kojidelmundo <koji@tanda.co>
2020-06-18 19:19:39 +0800

    fix spoopi info for mobile

--- src/components/SpoopiNav.js
+++ src/components/SpoopiNav.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./SpoopiNav.css"
 
 function SpoopiNav({ current_page }) {
+  const [showInfo, setShowInfo] = useState(window.innerWidth >= 800)
   const introText = `welcome to spoopi! here you can create timed spotify playlists. just give spoopi some categories and a duration and it'll find songs that fit into the time you gave it. have fun!`
+
+  const handleShowInfo = () => {
+    setShowInfo(!showInfo)
+  }
 
   return(
     <div className="SpoopiNav">
       <div className="spoopi-logo cursive">spoopi</div>
       <div className="spoopi-description">(spo)tify (p)laylist t(i)mer</div>
-    { current_page === "categories" && <div className="spoopi-intro cursive">{introText}</div> }
+      <div className={"spoopi-info" + (showInfo ? " info-clicked" : "")} onClick={handleShowInfo}>?</div>
+    { current_page === "categories" && showInfo &&
+      <div className="spoopi-intro cursive">{introText}</div> }
     </div>
   )
 }
